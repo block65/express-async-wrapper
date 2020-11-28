965a5bd23f454e460ee2394daa6de29ae2ffb29b
Max Holman <max@holmn.com>
2020-11-28 17:05:50 +0800

    feat: add optional generic for response type

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,22 @@
 import type { RequestHandler } from 'express';
+import type { JsonValue } from 'type-fest';
+// eslint-disable-next-line import/no-unresolved
+import * as core from 'express-serve-static-core';
 
-type AsyncRequestHandler = (
-  ...args: Parameters<RequestHandler>
+// eslint-disable-next-line @typescript-eslint/ban-types
+type ValidResponseBody<T> = Buffer | JsonValue | T;
+type ValidRequestBody = Buffer | JsonValue | any;
+
+type AsyncRequestHandler<P, ResBody, ReqBody, ReqQuery> = (
+  ...args: Parameters<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
 ) => Promise<void>;
 
-export function expressAsyncWrap(
-  fn: AsyncRequestHandler,
-): RequestHandler {
-  return (
-    req,
-    res,
-    next,
-  ): Promise<unknown> => {
-    return fn(req, res, next).catch(next);
-  };
+export function expressAsyncWrap<T = any>(
+  fn: AsyncRequestHandler<core.ParamsDictionary, T, ValidRequestBody, core.Query>,
+): RequestHandler<
+  core.ParamsDictionary,
+  ValidResponseBody<T>,
+  ValidRequestBody
+  > {
+  return (req, res, next): Promise<unknown> => fn(req, res, next).catch(next);
 }
