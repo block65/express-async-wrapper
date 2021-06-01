ae247ea4f10dfe2b040aefb1b1cc0a1592ec04ec
Max Holman <max@holmn.com>
2021-06-01 22:57:23 +0800

    chore: latest eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: '@block65/eslint-config',
+  root: true,
+  extends: ['@block65'],
+  parserOptions: {
+    project: ['tsconfig.json', '__tests__/tsconfig.json'],
+  },
 };
