09cce3eddc83e77d6b3dd211bb74d0b88e08e47b
Max Holman <max@holmn.com>
2021-06-01 22:57:12 +0800

    chore: types update for latest/lint

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,32 @@
 import type { RequestHandler } from 'express';
+import type * as core from 'express-serve-static-core';
 import type { JsonValue } from 'type-fest';
-// eslint-disable-next-line import/no-unresolved
-import * as core from 'express-serve-static-core';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type ValidResponseBody<T> = Buffer | JsonValue | T;
 type ValidRequestBody = Buffer | JsonValue | any;
 
-type AsyncRequestHandler<P, ResBody, ReqBody, ReqQuery> = (
-  ...args: Parameters<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
+type AsyncRequestHandler<
+  P = core.ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = core.Query,
+  Locals extends Record<string, any> = Record<string, any>,
+> = (
+  ...args: Parameters<RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>>
 ) => Promise<void>;
 
 export function expressAsyncWrap<T = any>(
-  fn: AsyncRequestHandler<core.ParamsDictionary, T, ValidRequestBody, core.Query>,
+  fn: AsyncRequestHandler<
+    core.ParamsDictionary,
+    T,
+    ValidRequestBody,
+    core.Query
+  >,
 ): RequestHandler<
   core.ParamsDictionary,
   ValidResponseBody<T>,
   ValidRequestBody
-  > {
+> {
   return (req, res, next): Promise<unknown> => fn(req, res, next).catch(next);
 }
